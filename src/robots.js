aa1269599f80c2be41b193dc18405e87c03e9b50
jerahmeelwilson <wilsonjerahmeel@gmail.com>
2025-01-05 18:38:25 -0600

    Initial commit

---
+++ src/robots.js
@@ -0,0 +1,68 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+const randomCats = 1000
+
+export const robots = [
+  {
+    id: getRandomInt(randomCats),
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+  },
+  {
+    id: getRandomInt(randomCats),
+    name: "Ervin Howell",
+    username: "Antonette",
+    email: "Shanna@melissa.tv",
+  },
+  {
+    id: getRandomInt(randomCats),
+    name: "Clementine Bauch",
+    username: "Samantha",
+    email: "Nathan@yesenia.net",
+  },
+  {
+    id: getRandomInt(randomCats),
+    name: "Patricia Lebsack",
+    username: "Karianne",
+    email: "Julianne.OConner@kory.org",
+  },
+  {
+    id: getRandomInt(randomCats),
+    name: "Chelsey Dietrich",
+    username: "Kamren",
+    email: "Lucio_Hettinger@annie.ca",
+  },
+  {
+    id: getRandomInt(randomCats),
+    name: "Mrs. Dennis Schulist",
+    username: "Leopoldo_Corkery",
+    email: "Karley_Dach@jasper.info",
+  },
+  {
+    id: getRandomInt(randomCats),
+    name: "Kurtis Weissnat",
+    username: "Elwyn.Skiles",
+    email: "Telly.Hoeger@billy.biz",
+  },
+  {
+    id: getRandomInt(randomCats),
+    name: "Nicholas Runolfsdottir V",
+    username: "Maxime_Nienow",
+    email: "Sherwood@rosamond.me",
+  },
+  {
+    id: getRandomInt(randomCats),
+    name: "Glenna Reichert",
+    username: "Delphine",
+    email: "Chaim_McDermott@dana.io",
+  },
+  {
+    id: getRandomInt(randomCats),
+    name: "Clementina DuBuque",
+    username: "Moriah.Stanton",
+    email: "Rey.Padberg@karina.biz",
+  },
+];
